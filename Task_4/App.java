1b4825ce10208587ef7ecb5b7e027221c13258d2
Alexei Yurikov <roboshadow@mail.ru>
2022-10-29 17:44:19 +0300

    add class App

---
+++ Task_4/App.java
@@ -0,0 +1,9 @@
+package Homework.Task_4;
+
+public class App {
+    public static void main(String[] args) {
+        Customer c = new Customer();
+//        c.searchTicket(Ticket date, Ticket time);
+
+    }
+}
