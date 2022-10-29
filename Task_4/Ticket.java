c5be9df9bdb5ca3cc60476fc87ac8de14cbe8f3e
Alexei Yurikov <roboshadow@mail.ru>
2022-10-29 14:27:20 +0300

    add class Ticket

--- Task_4/Ticket.java
+++ Task_4/Ticket.java
@@ -1,5 +1,14 @@
 package Homework.Task_4;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Ticket {
+    int rootNumber;
+    int place;
+    int price;
+    Date date;
+    Time time;
+    boolean isValid;
 
 }
