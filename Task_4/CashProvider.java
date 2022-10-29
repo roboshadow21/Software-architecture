70af7b7bf81322628660334cc7a554870b1427c2
Alexei Yurikov <roboshadow@mail.ru>
2022-10-29 14:50:13 +0300

    add class CashProvider

---
+++ Task_4/CashProvider.java
@@ -0,0 +1,15 @@
+package Homework.Task_4;
+
+public class CashProvider {
+    long card;
+    boolean isAuthorization;
+
+    public void buy(int price) {
+
+    }
+    public void authorization(Customer c) {
+
+    }
+
+
+}
