5f126e489735d9efc1b44236014aa9f39e20b5bc
Alexei Yurikov <roboshadow@mail.ru>
2022-10-29 14:56:19 +0300

    add class TicketProvider

---
+++ Task_4/TicketProvider.java
@@ -0,0 +1,11 @@
+package Homework.Task_4;
+
+public class TicketProvider {
+    public Ticket getTickets(Ticket rootNumber) {
+        return new Ticket();
+    }
+
+    public boolean updateTicketStatus(Ticket t) {
+        return true;
+    }
+}
